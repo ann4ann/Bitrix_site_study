eff5e3a8055a74c064fe8116fbfe60dac1960ca3
Anna Noskova <anni4anny@gmail.com>
2023-12-07 11:33:36 +0300

    task 156549

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -41,6 +41,7 @@
       $APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . "/js/main.js");
     ?>
   </head>
+  
   <body>
     <div id="panel"><?$APPLICATION->ShowPanel();?></div>
 
@@ -55,33 +56,84 @@
         <div class="site-mobile-menu-body"></div>
       </div>
       <!-- .site-mobile-menu -->
+
       <div class="border-bottom bg-white top-bar">
         <div class="container">
           <div class="row align-items-center">
             <div class="col-6 col-md-6">
               <p class="mb-0">
-                <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-                <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+                <?$APPLICATION->IncludeComponent(
+                  "bitrix:main.include",
+                  "",
+                  Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/phone.php"
+                  )
+                );?>
+                <?$APPLICATION->IncludeComponent(
+                  "bitrix:main.include",
+                  "",
+                  Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/email.php"
+                  )
+                );?>
               </p>
             </div>
             <div class="col-6 col-md-6 text-right">
-              <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-              <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-              <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
+              <?$APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                Array(
+                  "AREA_FILE_SHOW" => "file",
+                  "AREA_FILE_SUFFIX" => "inc",
+                  "EDIT_TEMPLATE" => "",
+                  "PATH" => "/include/socials.php"
+                )
+              );?>
             </div>
           </div>
         </div>
       </div>
+
       <div class="site-navbar">
         <div class="container py-1">
           <div class="row align-items-center">
             <div class="col-8 col-md-8 col-lg-4">
-              <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                class="text-danger">.</span></strong></a></h1>
+              <h1 class="">
+                <?$APPLICATION->IncludeComponent(
+                  "bitrix:main.include",
+                  "",
+                  Array(
+                    "AREA_FILE_SHOW" => "file",
+                    "AREA_FILE_SUFFIX" => "inc",
+                    "EDIT_TEMPLATE" => "",
+                    "PATH" => "/include/logo.php"
+                  )
+                );?>
+              </h1>
             </div>
             <div class="col-4 col-md-4 col-lg-8">
+              <?$APPLICATION->IncludeComponent(
+                "bitrix:menu",
+                "horizontal_multilevel",
+                Array(
+                  "ALLOW_MULTI_SELECT" => "N",
+                  "CHILD_MENU_TYPE" => "left",
+                  "DELAY" => "N",
+                  "MAX_LEVEL" => "2",
+                  "MENU_CACHE_GET_VARS" => array(""),
+                  "MENU_CACHE_TIME" => "3600",
+                  "MENU_CACHE_TYPE" => "N",
+                  "MENU_CACHE_USE_GROUPS" => "Y",
+                  "ROOT_MENU_TYPE" => "top",
+                  "USE_EXT" => "N"
+                )
+              );?>
               <nav class="site-navigation text-right text-md-right" role="navigation">
                 <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
                   class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
